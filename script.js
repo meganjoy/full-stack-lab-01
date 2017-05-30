890751e74705502d16cecd15a83799eb4e035fb5
meganjoy <meganjoybond@gmail.com>
2017-05-30 16:09:48 -0400

    Made bullets neater

--- script.js
+++ script.js
@@ -4,34 +4,33 @@ var career = 'Former teacher';
 console.log('Career: ' + career);
 var desc = 'Climber,  waterski-er, and reader';
 console.log('Description: ' + desc);
-
+console.log("");
 
 var interests = ['reading', 'writing', 'arithmetic'];
 console.log ("My interests: ")
-console.log(interests[0]);
-console.log(interests[1]);
-console.log(interests[2]);
-
+console.log('* ' + interests[0]);
+console.log('* ' + interests[1]);
+console.log('* ' + interests[2]);
+console.log("");
 
 console.log("My previous experience: ")
 function displayPosition (job, company, desc) {
-    console.log(job + ' at ' + company + ' -' + desc)
+    console.log('* ' + job + ' at ' + company + ' -' + desc)
 }
-displayPosition('head tech guru', 'Macbook Genius Bar', 'helped troubleshoot');
-displayPosition('teacher', 'Covalence', 'taught newbies some code skills');
-displayPosition('teacher', 'School somewhere', 'was patient and said things over and over');
-
+displayPosition('Head tech guru', 'Macbook Genius Bar', 'helped troubleshoot');
+displayPosition('Teacher', 'Covalence', 'taught newbies some code skills');
+displayPosition('Teacher', 'school somewhere', 'was patient and said things over and over');
+console.log("");
 
 
 console.log("My Skills: ")
 function displaySkill (skill, isCool) {
     if (isCool) {
-        console.log('BAM: ' + skill);
+        console.log('* BAM: ' + skill);
     } else {
-        console.log(skill);
+        console.log('* ' + skill);
     };
 }
-
 
 displaySkill('farmer', true);
 displaySkill('truant', false);
